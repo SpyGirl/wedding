151d24e4c9c12521a1d60dc618ab461230f11511
Dmitry Dyokin <Dmitry.Dyokin@ocsico.com>
2015-01-02 16:30:15 +0200

    DAO-layer is modified.
    Defined some methods in UserDao, and then they're implemented in UserDaoImpl.

--- Wedding/src/main/java/com/wedding/dao/UserDao.java
+++ Wedding/src/main/java/com/wedding/dao/UserDao.java
@@ -1,4 +1,17 @@
 package com.wedding.dao;
 
+import com.wedding.entity.Role;
+import com.wedding.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    User findByLogin(String login);
+    List<User> findByFirstName(String firstName);
+    List<User> findByLastName(String lastName);
+    List<User> findByFullName(String firstName, String lastName);
+    List<User> findByGender(Character gender);
+    User findByEmail(String email);
+    List<User> findByRole(Role role);
 }
--- Wedding/src/main/java/com/wedding/dao/impls/UserDaoImpl.java
+++ Wedding/src/main/java/com/wedding/dao/impls/UserDaoImpl.java
@@ -1,7 +1,72 @@
 package com.wedding.dao.impls;
 
 import com.wedding.dao.UserDao;
+import com.wedding.entity.Role;
+import com.wedding.entity.User;
+
+import java.util.List;
 
 public class UserDaoImpl extends BaseDaoImpl implements UserDao {
 
+    @Override
+    public User findByEmail(String email) {
+        User user = (User) session.createQuery("FROM User u WHERE u.email= :email")
+                .setParameter("email", email).uniqueResult();
+        return user;
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        User user = (User) session.createQuery("FROM User u WHERE u.login= :login")
+                .setParameter("login", login).uniqueResult();
+        return user;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findByFirstName(String firstName) {
+        List<User> users = session.createQuery("FROM User u WHERE u.firstName= :firstName")
+                .setParameter("firstName", firstName).list();
+        return users;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findByLastName(String lastName) {
+        List<User> users = session.createQuery("FROM User u WHERE u.lastName= :lastName")
+                .setParameter("lastName", lastName).list();
+        return users;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findByFullName(String firstName, String lastName) {
+        List<User> users = session.createQuery("FROM User u WHERE u.lastName= :lastName " +
+                "AND u.firstName= :firstName")
+                .setParameter("lastName", lastName)
+                .setParameter("firstName", firstName).list();
+        return users;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findByGender(Character gender) {
+        List<User> users = session.createQuery("FROM User u WHERE u.gender= :gender")
+                .setParameter("gender", gender).list();
+        return users;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findByRole(Role role) {
+        List<User> users;
+        if (role.getId() != null) {
+            users = session.createQuery("FROM User u JOIN Role r ON u.role.id = r.id").list();
+        } else if (role.getRoleName() != null) {
+            users = session.createQuery("FROM User u JOIN Role r ON u.role.roleName = r.roleName").list();
+        } else {
+            users = null;
+        }
+        return users;
+    }
 }
